b49c326c1af298b9e11684025969b11b9bcd90ba
tmaurie <thomas.maurier91@gmail.com>
2022-04-27 00:16:24 +0200

    [FIX] target static nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static', // default is 'server'
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - movixel',
