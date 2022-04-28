5a297b08632231c7bfdf2d658252773414a73764
tmaurie <thomas.maurier91@gmail.com>
2022-04-28 02:32:14 +0200

    [FEATURE] created game page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container class="justify-center" fill-height>
     <div class="d-flex flex-column">
-      <h1 class="mb-4 text-center title--movixel">MoviXel</h1>
-      <v-btn x-large color="primary" class="mb-4 rounded-xl">Play the game</v-btn>
+      <h1 class="mb-4 text-center title--movixel white--text">MoviXel</h1>
+      <v-btn x-large color="primary" class="mb-4 rounded-xl" to="/play">Play the game</v-btn>
       <v-btn x-large color="primary" disabled class="mb-4 rounded-xl">Daily challenge</v-btn>
       <v-btn x-large color="primary" disabled class="mb-4 rounded-xl">Settings</v-btn>
 
---
+++ pages/play.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+const API_KEY = process.env.VUE_APP_API_KEY
+
+export default {
+  name: "play",
+  data() {
+    return {
+      apiKey: 'tset'
+
+    }
+  },
+  mounted() {
+    console.log("api:" + API_KEY)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
